e51825a0933e432c2defd88ceaf634dfe2c72535
Ariba S <153156108+asiddiqi16@users.noreply.github.com>
2024-08-02 13:46:38 +1000

    Initial repo commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 class="">Dictionary</h1>
+      </header>
+      <footer>
+        <p>
+          This project has been created by Ariba Siddiqi, open sourced on{" "}
+          <a href="https://github.com/asiddiqi16/dictionary-app"> GitHub </a>{" "}
+          and hosted on
+          <a href="">Netlify</a>.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
